18e058fee8e7ae08b1e37cc5c89dfae7d6c0554b
AndyRama <andyramaroson@gmail.com>
2021-03-17 20:17:57 +0100

    add current_user | add create message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,20 @@
 class MessagesController < ApplicationController
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.chatroom= @chatroom
+    @message.user= current_user
+
+    if message.save
+       redirect_to chatroom_path(@chatroom, anchor:"message-#{message.id}")
+    else
+      render " chatroom/show"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:messages).permit(:content)
+  end
 end
