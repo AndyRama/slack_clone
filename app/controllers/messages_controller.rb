7e59361ba605f6f290b5cffb3566edd1e3b580f9
AndyRama <andyramaroson@gmail.com>
2021-03-18 10:35:43 +0100

    route nested

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,16 +5,16 @@ class MessagesController < ApplicationController
     @message.chatroom = @chatroom
     @message.user = current_user
 
-    if message.save
-      redirect_to chatroom_path(@chatroom, anchor:"message-#{message.id}")
+    if @message.save
+      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
     else
-      render " chatroom/show"
+      render "chatrooms/show"
     end
   end
 
   private
 
   def message_params
-    params.require(:messages).permit(:content)
+    params.require(:message).permit(:content)
   end
 end
