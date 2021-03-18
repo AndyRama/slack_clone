2a5963ba209eabc3a7907fe1ac4df07cfeca51ce
AndyRama <andyramaroson@gmail.com>
2021-03-18 10:10:58 +0100

    chatroom/messages Controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,11 @@ class MessagesController < ApplicationController
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
     @message = Message.new(message_params)
-    @message.chatroom= @chatroom
-    @message.user= current_user
+    @message.chatroom = @chatroom
+    @message.user = current_user
 
     if message.save
-       redirect_to chatroom_path(@chatroom, anchor:"message-#{message.id}")
+      redirect_to chatroom_path(@chatroom, anchor:"message-#{message.id}")
     else
       render " chatroom/show"
     end
